b4d40f8c57b5650ca10cc0825544c8424ba1529a
卫志龙 <1807149205@qq.com>
2022-06-08 12:27:59 +0800

    commit

--- Java/src/dao/FileIO.java
+++ Java/src/dao/FileIO.java
@@ -5,11 +5,7 @@ import java.io.*;
 public class FileIO {
 
     public void printFileInLocal(File file ,String text) throws IOException {
-//        FileWriter fileWriter = new FileWriter(file);
-//        fileWriter.write(text);
-//        fileWriter.close();
         OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
-        System.out.println(text);
         writer.write(text , 0 , text.length());
         writer.close();
     }
